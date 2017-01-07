5acf5eaadb7e68751cb4ceb1508f4e3fe4057255
suwananeye <suwanan225@gmail.com>
2017-01-07 11:52:02 +0700

    aa

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Http\Request;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/logout', function () {
+  Session::flush();
+    return view('welcome');
+});
 
 Route::group(['middleware' => 'web'], function() {
 
