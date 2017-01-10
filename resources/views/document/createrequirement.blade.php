688349fcab44c1e192cecd6eec7d3f4fe65741e5
suwananeye <suwanan225@gmail.com>
2017-01-10 10:57:49 +0700

    edit from document

--- resources/views/document/createrequirement.blade.php
+++ resources/views/document/createrequirement.blade.php
@@ -3,75 +3,61 @@
 @section('content')
 
 <div class="content-wrapper" style="height:1000px;">
+  <div class="container" style="padding-top:30px;">
+    <div class="col-md-2">
+      <!-- <a href="{{ url('profile/create') }}" class="btn btn-block btn-warning">Create</a> -->
+      <div class="container" style="padding-top:30px;">
+        <div class="col-md-8">
 
-
-
-<div class="container" style="padding-top:30px;"
-  <div class="col-md-2">
-<!-- <a href="{{ url('profile/create') }}" class="btn btn-block btn-warning">Create</a> -->
-<div class="container" style="padding-top:30px;">
-  <div class="col-md-8">
-
-
-</div>
-  <div class="row" style="padding-top:50px;">
-    <div class="col-xs-10 col-md-10">
-      <div class="box">
-        <div class="box-header">
-          <h3 class="box-title">Create Requirement</h3>
-          <div class="box-body ">
-
-<br>
-<br>
-            <div class="row">
-              <div class="col-md-4" style="text-align:right;">
-                <label for="firstname" >Title</label>
-              </div>
-                <div class="col-md-6">
-                <input class="form-control" type="text" id="Title"  name="Title" value="" placeholder="Title">
-              </div>
-              <div class="row">
-                <div class="col-md-4" style="text-align:right;">
-                  <label for="firstname" >Upload file</label>
-                </div>
-                <a  href="{{url('document/uploadfile')}}" class="btn  btn-warning" style=" margin-left: 20px">Uploadfile</a>
-
-                            </div>
-
-<br>
-<br>
-<br>
-<div class="row">
-              <center>
-                 <button type="submit" class="btn btn-primary">Cancel</button>
-                <button type="submit" class="btn btn-primary">Save</button>
-              </div></center>
-</div>
-
-            </div>
-
-
-
-
-
-
-
-
-
-
-          </div>
 
         </div>
+        <div class="row" style="padding-top:50px;">
+          <div class="col-xs-10 col-md-10">
+            <div class="box">
+              <div class="box-header">
+                <h3 class="box-title">Create Requirement</h3>
+                <div class="box-body ">
+                  <div class="form-group">
+                    <div class="row">
+                      <div class="col-md-4 col-xs-3" style="text-align:right;">
+                        <label for="firstname" >Title</label>
+                      </div>
+                      <div class="col-md-6 col-xs-9">
+                        <input class="form-control" type="text" id="Title"  name="Title" value="" placeholder="Title">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="row">
+                      <div class="col-md-4 col-xs-3" style="text-align:right;">
+                        <label for="firstname" >Upload file</label>
+                      </div>
+                      <div class="col-md-6 col-xs-9">
+                        <a  href="{{url('document/uploadfile')}}" class="btn  btn-warning" style="">Uploadfile</a>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="row">
+                      <div class="col-md-4 col-xs-3" style="text-align:right;">
+                      </div>
+                      <div class="col-md-6 col-xs-9">
+                        <button type="submit" class="btn btn-primary">Cancel</button>
+                        <button type="submit" class="btn btn-primary">Save</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         <!-- /.box-header -->
 
         <!-- /.box-body -->
+        </div>
       </div>
-
-
-</div>
-
-</div>
-</div>
+    </div>
+  </div>
 </div>
 
 
